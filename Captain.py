b1260d46a17705e870c275aefe19949f3a727dfd
aishu0619 <151384149+aishu0619@users.noreply.github.com>
2023-12-05 22:29:11 -0500

    Update Captain.py

--- Captain.py
+++ Captain.py
@@ -2,6 +2,11 @@ from Creature import Creature
 
 class Captain(Creature):
     def __init__(self, x_axis, y_axis):
+        """A function to get x and y coordinates of the captain
+        :param x_axis: x coordinate of the captain
+        :type x_axis: integer
+        :param x_axis: x coordinate of the captain
+        :type x_axis: integer"""
         super().__init__(x_axis, y_axis,"V")
         self._collected_veggie = []
 
