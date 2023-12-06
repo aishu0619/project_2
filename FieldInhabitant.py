a1cd727a83d7f6a3f62a192732cedceb9f024eb2
sreramv23 <144264445+sreramv23@users.noreply.github.com>
2023-12-05 22:42:45 -0500

    Update FieldInhabitant.py

--- FieldInhabitant.py
+++ FieldInhabitant.py
@@ -1,3 +1,8 @@
+# Author : Sreram Vasudev
+# Date : 12/5/2023
+# Description : This program that takes in the value for the text symbol and assigns
+# the value to the new member variables.
+
 class FieldInhabitant:
     def __init__(self,symbol):
         """A function to get the text symbol representing the field inhabitant
