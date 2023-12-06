fcda4b7c80b58299c227ed8a8b9381ec5fc833c4
aishu0619 <151384149+aishu0619@users.noreply.github.com>
2023-12-05 22:30:32 -0500

    Update Rabbit.py

--- Rabbit.py
+++ Rabbit.py
@@ -2,9 +2,13 @@ from Creature import Creature
 
 class Rabbit(Creature):
     def __init__(self, x_axis, y_axis):
+        """A function to get x and y coordinates of the rabbit
+        :param x_axis: x coordinate of the rabbit
+        :type x_axis: integer
+        :param x_axis: x coordinate of the rabbit
+        :type x_axis: integer"""
         super().__init__(x_axis, y_axis,"R")
 
     def updateLocation(self, newXaxis, newYaxis):
         self.x_axis = newXaxis
         self.y_axis = newYaxis  
-        
